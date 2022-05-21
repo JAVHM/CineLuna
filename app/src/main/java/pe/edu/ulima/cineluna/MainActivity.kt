d985b54995a9f7473ef07e1ffd6143e9d3e7fdf9
JAVHM <55992367+JAVHM@users.noreply.github.com>
2022-05-21 17:00:59 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/ulima/cineluna/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.edu.ulima.cineluna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Test
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
