cee2e1071fd524527c2181f798f23f19021510f5
SebasLV12 <sebasr_12@hotmail.com>
2022-05-22 02:26:17 -0500

    100000000000000/10000000000000000000000000000000

--- app/src/main/java/pe/edu/ulima/cineluna/LoginActivity.kt
+++ app/src/main/java/pe/edu/ulima/cineluna/LoginActivity.kt
@@ -1,11 +1,43 @@
 package pe.edu.ulima.cineluna
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class LoginActivity : AppCompatActivity() {
+    private var mButLogin: Button?=null
+     var eteLoginNombre: TextView?=null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+    //Obtener referencia
+    mButLogin=findViewById(R.id.butLogin)
+    eteLoginNombre=findViewById<EditText>(R.id.eteLoginNombre)
+    val resultLauncher=registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result->
+        Log.i("login","main activity")
+
+    }
+
+    //SET ONCLICK
+    mButLogin!!.setOnClickListener{v: View ->
+
+        val intent=Intent()
+        intent.putExtra("NOMBRE", eteLoginNombre!!.text.toString())
+        setResult(RESULT_OK,intent)
+        intent.setClass(this,MainActivity::class.java)
+        resultLauncher.launch(intent)
+
+
+    }
+
     }
 }
--- app/src/main/java/pe/edu/ulima/cineluna/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/cineluna/MainActivity.kt
@@ -2,12 +2,30 @@ package pe.edu.ulima.cineluna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : FragmentActivity() {
+     var eteNombre: TextView?=null;
+     var eteSaludo:TextView?=null;
+     var menu: NavigationView?=null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //Test 12121212
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Test 12121212
+        menu= findViewById(R.id.nviMain);
+        //headerLayout=NavigationView!!.getHeaderView(0);
+        eteNombre = menu!!.getHeaderView(0).findViewById(R.id.eteNombre)
+        eteSaludo=findViewById<TextView>(R.id.tviTitulo)
+
+        val bundle=intent.extras
+        val dato=bundle?.getString("NOMBRE")
+        eteSaludo!!.text="Hola ${dato.toString()}"
+        eteNombre!!.setText(dato.toString())
+
     }
 }
